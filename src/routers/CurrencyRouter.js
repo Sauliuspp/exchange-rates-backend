f331b9c93ee16348d7dcaeef32f974b5f8e9af24
Sauliuspp <pas.saulius@gmail.com>
2023-12-17 10:01:39 +0200

    Changes import statement grouping

--- src/routers/CurrencyRouter.js
+++ src/routers/CurrencyRouter.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 
 const { asyncWrap } = require('../Utils');
 
-const exchangeRateDb = require('../databases/ExchangeRateDatabase');
 const LogRepository = require('../repositories/LogRepository');
 const CurrencyService = require('../services/CurrencyService');
 const CurrencyController = require('../controllers/CurrencyController');
 
+const exchangeRateDb = require('../databases/ExchangeRateDatabase');
 const cacheApi = require('../api/CacheApi');
 const currencyApi = require('../api/CurrencyApi');
+
 const logRepository = new LogRepository({ exchangeRateDb });
 const currencyService = new CurrencyService({
     cacheApi,
